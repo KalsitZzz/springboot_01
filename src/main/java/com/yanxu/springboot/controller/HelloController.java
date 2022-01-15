3b80925bd6f145512dda065d448ac2f68cf98916
闫旭 <1406076781@qq.com>
2022-01-15 16:56:08 +0800

    ver7.0

--- src/main/java/com/yanxu/springboot/controller/HelloController.java
+++ src/main/java/com/yanxu/springboot/controller/HelloController.java
@@ -7,7 +7,7 @@ import com.yanxu.springboot.properties.JdbcProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//morning
 @RestController
 public class HelloController {
     @Autowired
--- src/main/java/com/yanxu/springboot/controller/TestController.java
+++ src/main/java/com/yanxu/springboot/controller/TestController.java
@@ -3,5 +3,6 @@ package com.yanxu.springboot.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("ssss");
+        System.out.println("args = " + args);
     }
 }
