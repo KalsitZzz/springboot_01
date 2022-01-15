94358eb59c931c705bdeda89fbef92546f8e620c
闫旭 <1406076781@qq.com>
2022-01-15 17:01:54 +0800

    ver8.0

--- src/main/java/com/yanxu/springboot/controller/TestController.java
+++ src/main/java/com/yanxu/springboot/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("ssss");
         System.out.println("args = " + args);
+        System.out.println("oooooooooooo");
     }
 }
