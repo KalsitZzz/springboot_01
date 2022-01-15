8a3feb4a53591eddf571e61480d30fada2fed1fa
闫旭 <1406076781@qq.com>
2022-01-15 16:55:34 +0800

    ver6.0

--- src/main/java/com/yanxu/springboot/controller/HelloController.java
+++ src/main/java/com/yanxu/springboot/controller/HelloController.java
@@ -7,8 +7,7 @@ import com.yanxu.springboot.properties.JdbcProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//morningEveryOneButIt'sEvening
-//morningEveryOneButIt'sMorning
+
 @RestController
 public class HelloController {
     @Autowired
